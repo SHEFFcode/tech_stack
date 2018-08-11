81d1c4ae0d1bd091d1cae1c4a7ef6f0639c1bd84
SHEFFmachine <sheff@sheffmachine.com>
2018-08-11 10:06:00 -0700

    Fix up the styling

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react'
 import { Text } from 'react-native'
 import { CardSection } from '../common'
 
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 18,
+  },
+}
+
 class ListItem extends Component {
   render() {
     const { library } = this.props
+    const { titleStyle } = styles
     console.log(library)
 
     return (
       <CardSection>
-        <Text>{library.item.title}</Text>
+        <Text style={titleStyle}>{library.item.title}</Text>
       </CardSection>
     )
   }
